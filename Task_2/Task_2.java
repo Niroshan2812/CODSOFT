c03bc0b76e43c7e900d630045991c80f23c9d600
Niroshan <niroshan9812@gmail.com>
2025-04-19 13:20:56 +0530

    get user input with validation

--- Task_2/Task_2.java
+++ Task_2/Task_2.java
@@ -1,7 +1,39 @@
 package Task_2;
 
+import java.util.Scanner;
+
 public class Task_2 {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        UXhelper ux = new UXhelper();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(ux.askName);
+        String studentName =  sc.nextLine();
+
+        Strudent student = new Strudent(studentName);
+
+        System.out.println(ux.askNumberofSub);
+        int subcount = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i<subcount; i++){
+            System.out.println(ux.asknameofSubject + (i+1)+ "");
+            String subjectName = sc.nextLine();
+
+            int marks;
+            while(true){
+                System.out.println(ux.markEach + subjectName + ux.condition);
+                marks = sc.nextInt();
+                sc.nextLine();
+                if(marks>= 0 && marks <= 100){
+                    break;
+                }
+                System.out.println(ux.invalidMarkInput);
+
+            }
+
+        }
     }
 }
--- Task_2/UXhelper.java
+++ Task_2/UXhelper.java
@@ -1,4 +1,13 @@
 package Task_2;
 
 public class UXhelper {
+    //worning
+    public final String invalidMarkInput = "Invalid inout, marks should be between 0 and 100 ";
+
+    //help
+    public final String askName = " Please enter student name ";
+    public final String askNumberofSub = "Enter number of subjects";
+    public final String asknameofSubject = "Enter name of Subject" ;
+    public final String markEach = "Enter marks For : ";
+    public final String condition = "(Out of 100) : ";
 }
