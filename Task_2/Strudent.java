857035780fbde30cbff30a86d3aecd2103233794
Niroshan <niroshan9812@gmail.com>
2025-04-19 12:15:01 +0530

    obbject defin

---
+++ Task_2/Strudent.java
@@ -0,0 +1,13 @@
+package Task_2;
+
+import java.util.ArrayList;
+
+public class Strudent {
+    private String name;
+    private ArrayList<Subject> subjects;
+
+    public Strudent(String name){
+        this.name = name;
+        this.subjects = new ArrayList<>();
+    }
+}
