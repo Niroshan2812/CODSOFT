dc022d2c0c88680d734cd6102fd04bb6719a938f
Niroshan <niroshan9812@gmail.com>
2025-04-28 13:41:02 +0530

    Task_4 intial commit

---
+++ Task_4/Task_4.java
@@ -0,0 +1,25 @@
+package Task_4;
+
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        UXhelper ux = new UXhelper();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(ux.welcome);
+        System.out.println(ux.askbCurrancyCode);
+        String baseCurrancy = sc.nextLine();
+
+        System.out.println(ux.asktCurrancyCode);
+        String targetCurrancy = sc.nextLine();
+
+        System.out.println(ux.askAmout);
+        double amount = sc.nextDouble();
+
+        
+
+
+    }
+}
---
+++ Task_4/UXhelper.java
@@ -0,0 +1,10 @@
+package Task_4;
+
+public class UXhelper{
+    public String welcome = "----------------------------------- Welcome to Currency Converter! ----------------------------------\n ";
+    public String askbCurrancyCode = "Enter base currency code (Ex: LKR): ";
+    public String asktCurrancyCode = "Enter target currency  code (Ex: USD): ";
+    public String askAmout = "Enter amount to convert: ";
+
+}
+
