f87ca6192ce97fc2fa1003504bbb4ec3d929b108
Niroshan <niroshan9812@gmail.com>
2025-05-04 03:43:23 +0530

    Include FXML and dashboard UI

---
+++ Task_5/src/com/Controller.java
@@ -0,0 +1,12 @@
+package Task_5.src.com;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private void handleButtonClick(ActionEvent event) {
+        System.out.println("Button clicked");
+    }
+}
