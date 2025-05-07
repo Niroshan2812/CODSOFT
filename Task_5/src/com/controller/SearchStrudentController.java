701d11e812436cc8b47a015eb6f8c680f0d0bb2a
Niroshan <niroshan9812@gmail.com>
2025-05-07 11:56:15 +0530

    Student search option included

--- Task_5/src/com/controller/SearchStrudentController.java
+++ Task_5/src/com/controller/SearchStrudentController.java
@@ -1,8 +1,12 @@
 package Task_5.src.com.controller;
 
+import Task_5.src.com.model.Student;
+import Task_5.src.com.searvice.StudentManagmentSystem;
+import Task_5.src.com.util.DBinitilizer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
@@ -10,9 +14,9 @@ import javafx.scene.control.Label;
 
 public class SearchStrudentController {
     @FXML private TextField searchField;
-    @FXML private Label ShowDetails;
     @FXML private Label messageLabel;
     @FXML private Button searchButton;
+    @FXML private TextArea showDetails;
 
 
 @FXML
@@ -21,7 +25,23 @@ private void handleSearch() {
 
     if(searchTerm.isEmpty()){
         messageLabel.setText("Please enter a search term");
+        showDetails.setText("");
+        return;
     }
+    Student student = StudentManagmentSystem.getStudentByRollNumber(searchTerm);
+    if(student != null){
+        showDetails.setText("Roll Number: "+ student.getRollNumber() + "\n"+
+                "Name: "+ student.getName() + "\n" +
+                "Grade: "+ student.getGrade() + "\n"+
+                "Contact Number: "+ student.getContactNumber()+ "\n"+
+                "Email: "+ student.getEmail()
+                );
+        messageLabel.setText("");
+    }else {
+        showDetails.setText("");
+        messageLabel.setText("Student not found");
+    }
+
 
 
 }
